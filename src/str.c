dc2e6b9cda3bd65a9bbf513c3d478ae7ec04017b
aiocat <aiocat@protonmail.com>
2022-05-28 16:50:00 +0300

    format code

--- src/str.c
+++ src/str.c
@@ -1,21 +1,23 @@
 /**
  * Copyright (c) 2022 aiocat
- * 
+ *
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
 
 #include "str.h"
 
-struct str* str_new() {
+struct str *str_new()
+{
     // create new str
-    struct str* str = malloc(sizeof(struct str));
+    struct str *str = malloc(sizeof(struct str));
     str->capacity = sizeof(char) * STR_CAP_S;
     str->length = 0;
     str->memory = malloc(str->capacity);
 
     // check if null
-    if (str->memory == NULL) {
+    if (str->memory == NULL)
+    {
         return NULL;
     }
 
@@ -23,23 +25,27 @@ struct str* str_new() {
     return str;
 }
 
-uint8_t str_push(struct str* str, const char* cstr) {
+uint8_t str_push(struct str *str, const char *cstr)
+{
     // read c-style string and append to str
-    for (size_t length = 0; cstr[length] != '\0'; length++) {
+    for (size_t length = 0; cstr[length] != '\0'; length++)
+    {
         // re-alloc if no memory avaible for next push
         size_t avaible_mem = str->capacity - str->length;
-        if (avaible_mem <= 1) {
+        if (avaible_mem <= 1)
+        {
             str->capacity += sizeof(char) * STR_CAP_S;
-            char* reallocted = realloc(str->memory, str->capacity);
+            char *reallocted = realloc(str->memory, str->capacity);
 
-            if (reallocted == NULL) {
+            if (reallocted == NULL)
+            {
                 *(str->memory + (str->length)) = 0;
                 return 0;
             }
 
             str->memory = reallocted;
         }
-        
+
         // push character
         *(str->memory + (str->length++)) = cstr[length];
     }
@@ -49,9 +55,11 @@ uint8_t str_push(struct str* str, const char* cstr) {
     return 1;
 }
 
-char str_pop(struct str* str) {
+char str_pop(struct str *str)
+{
     // check if string empty
-    if (str->length == 0) {
+    if (str->length == 0)
+    {
         return 0;
     }
 
@@ -61,16 +69,19 @@ char str_pop(struct str* str) {
     return variable;
 }
 
-char str_last(struct str* str) {
+char str_last(struct str *str)
+{
     // check if string empty
-    if (str->length == 0) {
+    if (str->length == 0)
+    {
         return 0;
     }
 
     return str->memory[str->length - 1];
 }
 
-void str_free(struct str* str) {
+void str_free(struct str *str)
+{
     str->capacity = 0;
     str->length = 0;
 
@@ -78,43 +89,54 @@ void str_free(struct str* str) {
     free(str);
 }
 
-struct str* str_from(const char* cstr) {
+struct str *str_from(const char *cstr)
+{
     // create new str
-    struct str* string = str_new();
+    struct str *string = str_new();
 
     // check if null
-    if (string == NULL) {
+    if (string == NULL)
+    {
         return NULL;
     }
 
     // push string
-    if (str_push(string, cstr) == 0) {
+    if (str_push(string, cstr) == 0)
+    {
         return NULL;
     }
 
     return string;
 }
 
-uint8_t str_is_empty(const struct str* str) {
-    if (str->length == 0) {
+uint8_t str_is_empty(const struct str *str)
+{
+    if (str->length == 0)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-char str_at(struct str* str, size_t index) {
+char str_at(struct str *str, size_t index)
+{
     // check index size
-    if (index >= str->length) {
+    if (index >= str->length)
+    {
         return 0;
     }
-    
+
     return str->memory[index];
 }
 
-char str_remove_at(struct str* str, size_t index) {
+char str_remove_at(struct str *str, size_t index)
+{
     // check index size
-    if (index >= str->length) {
+    if (index >= str->length)
+    {
         return 0;
     }
 
@@ -122,18 +144,20 @@ char str_remove_at(struct str* str, size_t index) {
 
     // remove index and decrease length
     char *clone;
-    for (clone = (str->memory)+index; *clone != '\0'; *clone = *(clone+1), ++clone);
+    for (clone = (str->memory) + index; *clone != '\0'; *clone = *(clone + 1), ++clone)
+        ;
     *clone = '\0';
     str->length--;
 
-    
     return will_deleted;
 }
 
-size_t str_length(const struct str* str) {
+size_t str_length(const struct str *str)
+{
     return str->length;
 }
 
-size_t str_capacity(const struct str* str) {
+size_t str_capacity(const struct str *str)
+{
     return str->capacity;
 }
--- src/str.h
+++ src/str.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 aiocat
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
@@ -8,7 +8,6 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 
 // define string capacity size
 #ifndef STR_CAP_S
@@ -16,43 +15,44 @@
 #endif
 
 // This struct is contains allocted memory, length and capacity.
-struct str {
-    char* memory; // for string memory.
-    size_t length; // for string length.
+struct str
+{
+    char *memory;    // for string memory.
+    size_t length;   // for string length.
     size_t capacity; // for memory capacity.
 };
 
 // create new empty string. will return null if fails.
-struct str* str_new();
+struct str *str_new();
 
 // create string from c-style string. will return null if fails.
-struct str* str_from(const char* cstr);
+struct str *str_from(const char *cstr);
 
-// push c-style string. will realloc if too big for struct. returns 1 (one) if function returns successfully, otherwise it will return 0 (zero). this operation is O(n).
-uint8_t str_push(struct str* str, const char* cstr);
+// push c-style string. will realloc if too big for struct. returns 1 (one) if function returns successfully, otherwise it will return 0 (zero).
+uint8_t str_push(struct str *str, const char *cstr);
 
 // remove last char and return it. returns \0 (zero) if string is empty.
-char str_pop(struct str* str);
+char str_pop(struct str *str);
 
 // get last char and return it. returns \0 (zero) if string is empty.
-char str_last(struct str* str);
+char str_last(struct str *str);
 
 // safely de-alloc string from memory. use this when all of the operations done.
-void str_free(struct str* str);
+void str_free(struct str *str);
 
 // check if string is empty. will return 1 (one) if empty, otherwise it will return 0 (zero).
-uint8_t str_is_empty(const struct str* str);
+uint8_t str_is_empty(const struct str *str);
 
 // return string length.
-size_t str_length(const struct str* str);
+size_t str_length(const struct str *str);
 
 // return string capacity.
-size_t str_capacity(const struct str* str);
+size_t str_capacity(const struct str *str);
 
 // get character by index and return it. will return \0 (zero) if fails.
-char str_at(struct str* str, size_t index);
+char str_at(struct str *str, size_t index);
 
 // remove character in given index and return it. will return \0 (zero) if fails.
-char str_remove_at(struct str* str, size_t index);
+char str_remove_at(struct str *str, size_t index);
 
 #endif /* STR_H */
